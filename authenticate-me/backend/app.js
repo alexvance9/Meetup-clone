7ba9b387f15e4c10610c7871cefa767086c6cf3b
alexvance9 <alexvance9@gmail.com>
2022-11-16 12:40:37 -0800

    finished backend setup and tested csrf

---
+++ authenticate-me/backend/app.js
@@ -0,0 +1,53 @@
+// import packages
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const routes = require('./routes');
+
+// check if environment is in production or not
+const { environment } = require('./config');
+const isProduction = environment === 'production';
+
+// initialize app
+const app = express();
+
+// connect morgan middleware- for logging info about requests and responses
+app.use(morgan('dev'));
+
+// for parsing:
+app.use(cookieParser());
+app.use(express.json());
+
+// Security Middleware
+if (!isProduction) {
+    // enable cors only in development
+    app.use(cors());
+}
+
+// helmet helps set a variety of headers to better secure your app
+app.use(
+    helmet.crossOriginResourcePolicy({
+        policy: "cross-origin"
+    })
+);
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
+);
+
+// connect all the routes
+app.use(routes);
+
+
+module.exports = app;
