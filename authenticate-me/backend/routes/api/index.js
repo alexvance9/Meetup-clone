6d857698d3212aa55604aad27619f71f35dbc344
alexvance9 <alexvance9@gmail.com>
2022-11-17 14:50:58 -0800

    Add User authentification Express middleware, & test

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -1,10 +1,50 @@
 const router = require('express').Router();
 
-router.post('/test', function (req, res) {
-    res.json({ requestBody: req.body });
-});
+const { restoreUser } = require('../../utils/auth.js');
+
+// Connect restoreUser middleware to the API router
+// If current user session is valid, set req.user to the user in the database
+// If current user session is not valid, set req.user to null
+router.use(restoreUser);
 
 
-// n7Dy7VX3 - Nk - 8q02bEjLREu7LvjdP0NcYq64
+// GET /api/restore-user
+
+// test route 
+
+// test route
+// GET /api/require-auth
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//     '/require-auth',
+//     requireAuth,
+//     (req, res) => {
+//         return res.json(req.user);
+//     }
+// );
+
+// test route
+// router.post('/test', function (req, res) {
+//     res.json({ requestBody: req.body });
+// });
+
+// TEST setTokenCookie function
+// GET /api/set-token-cookie
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', async (_req, res) => {
+//     const user = await User.findOne({
+//         where: {
+//             username: 'Demo-lition'
+//         }
+//     });
+//     setTokenCookie(res, user);
+//     return res.json({ user: user });
+// });
+
+
+
+
+
 
 module.exports = router;
---
+++ authenticate-me/backend/utils/auth.js
@@ -0,0 +1,74 @@
+// User Auth Middlewares
+
+const jwt = require('jsonwebtoken');
+const { jwtConfig } = require('../config');
+const { User } = require('../db/models');
+
+const { secret, expiresIn } = jwtConfig;
+
+// function to be used in login and signup routes.
+// Sends a JWT Cookie
+const setTokenCookie = (res, user) => {
+    // Create the token.
+    const token = jwt.sign(
+        { data: user.toSafeObject() },
+        secret,
+        { expiresIn: parseInt(expiresIn) } // 604,800 seconds = 1 week
+    );
+
+    const isProduction = process.env.NODE_ENV === "production";
+
+    // Set the token cookie
+    res.cookie('token', token, {
+        maxAge: expiresIn * 1000, // maxAge in milliseconds
+        httpOnly: true,
+        secure: isProduction,
+        sameSite: isProduction && "Lax"
+    });
+
+    return token;
+};
+
+
+// will be connected to API router so all API route handlers will check for current logged in user.
+const restoreUser = (req, res, next) => {
+    // token parsed from cookies
+    const { token } = req.cookies;
+    req.user = null;
+
+    return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+        if (err) {
+            return next();
+        }
+
+        try {
+            const { id } = jwtPayload.data;
+            req.user = await User.scope('currentUser').findByPk(id);
+        } catch (e) {
+            res.clearCookie('token');
+            return next();
+        }
+
+        if (!req.user) res.clearCookie('token');
+
+        return next();
+    });
+};
+
+// read.me says to put restoreUser as first ele in array with requireAuth, but code snippet was different.
+//  we will see if causes probs later.
+
+// will be connected directly to routehandlers where there needs to be a current user logged in for the
+// actions in those route handlers.
+// If there is no current user, return an error
+const requireAuth = function (req, _res, next) {
+    if (req.user) return next();
+
+    const err = new Error('Authentication required');
+    err.title = 'Authentication required';
+    err.errors = ['Authentication required'];
+    err.status = 401;
+    return next(err);
+}
+
+module.exports = { setTokenCookie, restoreUser, requireAuth };
