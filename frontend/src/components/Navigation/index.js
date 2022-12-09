ef07ba8c173b289895ca4ec80b34f9dc502d937b
alexvance9 <alexvance9@gmail.com>
2022-12-09 14:26:23 -0800

    profile dropdown and logout functionality

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,18 +1,11 @@
-import './Navigation.css';
-
-
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
-import * as sessionActions from '../../store/session';
 import './Navigation.css';
 
 function Navigation({ isLoaded }) {
     const sessionUser = useSelector(state => state.session.user);
-    const dispatch = useDispatch();
-
-
 
     let sessionLinks;
     if (sessionUser) {
