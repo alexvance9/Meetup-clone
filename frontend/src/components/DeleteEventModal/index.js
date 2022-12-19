f97a24ef6287d30227f1bb16aec343ca6d81aec4
alexvance9 <alexvance9@gmail.com>
2022-12-18 17:22:07 -0800

    error handling back-ups

--- frontend/src/components/DeleteEventModal/index.js
+++ frontend/src/components/DeleteEventModal/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
@@ -6,6 +7,7 @@ import { thunkDeleteEvent } from '../../store/events';
 function DeleteEventModal({ currentEventId }) {
     const dispatch = useDispatch();
     const history = useHistory();
+    const [errors, setErrors] = useState([])
 
     // add error handling here
 
@@ -13,17 +15,24 @@ function DeleteEventModal({ currentEventId }) {
 
     const handleDelete = async (e) => {
         e.preventDefault();
+        const errors = [];
 
         const deleteEvent = await dispatch(thunkDeleteEvent(currentEventId))
 
         if (deleteEvent.ok) {
             closeModal()
             history.push('/home')
-        } 
+        } else {
+            deleteEvent.errors.forEach(error => errors.push(error))
+            setErrors(errors)
+        }
     }
 
     return (
         <div className="delete-modal">
+            <ul>
+                {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+            </ul>
             <h2>Are you sure you want to delete this Event?</h2>
             <h3>This will also delete all associated Photos.</h3>
             <button onClick={handleDelete}>Yes, Delete it.</button>
