64d59e8191eaacb0f15635ed5161e846439a5f19
alexvance9 <alexvance9@gmail.com>
2023-02-21 12:51:11 -0800

    group seeders adjusted

--- backend/db/seeders/20221121235858-demo-group-image.js
+++ backend/db/seeders/20221121235858-demo-group-image.js
@@ -12,23 +12,23 @@ module.exports = {
     return queryInterface.bulkInsert(options, [
       {
         groupId: 1,
-        url: 'sample.com/sampleimg1',
+        url: 'https://w0.peakpx.com/wallpaper/261/470/HD-wallpaper-alex-flame-names-name-human-name-design-people-person-name-your-names.jpg',
         preview: true
       },
       {
         groupId: 1,
-        url: 'sample.com/sampleimg2',
+        url: 'https://townsquare.media/site/87/files/2014/11/Vevo-via-YouTube.png?w=980&q=75',
         preview: false
       },
       {
         groupId: 2,
-        url: 'sample.com/sampleimg3',
+        url: 'https://chefsmandala.com/wp-content/uploads/2018/03/corn-600x338.jpg',
         preview: true
       },
       {
         groupId: 3,
-        url: 'sample.com/sampleimg4',
-        preview: false
+        url: 'https://miro.medium.com/max/1200/1*DTrU08K4jbRaq33u1PIFIA.jpeg',
+        preview: true
       },
     ])
   
