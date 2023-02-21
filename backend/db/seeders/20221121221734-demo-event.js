5791f41397091f211151c991892b06a6f8a1b07b
alexvance9 <alexvance9@gmail.com>
2023-02-21 14:25:38 -0800

    adjusted event seeders

--- backend/db/seeders/20221121221734-demo-event.js
+++ backend/db/seeders/20221121221734-demo-event.js
@@ -13,35 +13,35 @@ module.exports = {
       {
         groupId: 1,
         venueId: 1,
-        name: "Demo Event",
+        name: "Alex Hangout",
         type: "Online",
         capacity: 10,
         price: 1.50,
-        description: "A really fun event",
-        startDate: new Date(2070, 1, 1),
-        endDate: new Date(2071, 1, 1)
+        description: "A really fun event where all of us Alex's get together and gab about Alex related topics. Topics including: What does an Alex like to eat? How should an Alex interact with non-Alex's? Where does an Alex come from? Where does an Alex go?",
+        startDate: new Date(2023, 3, 3),
+        endDate: new Date(2023, 3, 4)
       },
       {
         groupId: 1,
         venueId: 2,
-        name: "Demo Event 2",
+        name: "Alex's being Alex's: Panel Discussion",
         type: "Online",
         capacity: 10,
         price: 1.50,
-        description: "a way more fun event",
-        startDate: new Date(2070, 1, 1),
-        endDate: new Date(2071, 1, 1)
+        description: "A panel of our best and brightest Alex's discuss getting ahead and staying ahead.",
+        startDate: new Date(2023, 3, 5),
+        endDate: new Date(2023, 3, 6)
       },
       {
         groupId: 2,
         venueId: 3,
-        name: "Corn Event",
+        name: "Corntastic!",
         type: "In Person",
         capacity: 10,
         price: 1.50,
-        description: "What we talk about when we talk about corn",
-        startDate: new Date(2070, 1, 1),
-        endDate: new Date(2071, 1, 1)
+        description: "What we talk about when we talk about corn! As always, we will have a super corn-y potluck, everyone is welcome to participate! Bring your favorite corn dish and be ready to talk corn varieties with special guest Dr. Corn-ell West!",
+        startDate: new Date(2023, 3, 2),
+        endDate: new Date(2071, 3, 3)
       },
       {
         groupId: 3,
@@ -50,9 +50,9 @@ module.exports = {
         type: "Online",
         capacity: 10,
         price: 1.50,
-        description: "Topic: Will we ever find jobs?",
-        startDate: new Date(2070, 1, 1),
-        endDate: new Date(2071, 1, 1)
+        description: "This week's topic: Will we ever find jobs? We invited back some past bootcamp grads who have been on the other side of the job search. As they've all been recently laid off, they have lots of time to meet with everyone and lend their support... and we agreed to let them pitch their start-up ideas! Fun for one and all!",
+        startDate: new Date(2023, 3, 1),
+        endDate: new Date(2023, 3, 2)
       }
     ])
   },
